e406d776ad48808c5209d5eb201d456971acc28f
javiercordero <javier@imaginary.tech>
2015-11-01 16:58:26 -0400

    v1.2a Added pause on scrollbar, bug fixes, touchscereen and multi monitor support in progress.

--- animate_scroll.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('scroll_anim', function( offset )
-	{
-		$("html, body").animate({ scrollTop: "+=" + offset }, 40, function()
-			{
-				self.scroll_anim();
-			});
-}, false);
