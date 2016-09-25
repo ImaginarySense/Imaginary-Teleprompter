bb7673331ba974c1311e9c50da9353f8b336ccd7
Rafael Sierra <developersystems9@gmail.com>
2016-09-24 23:03:45 -0400

    Fix OpenExternal Links && updated code in Main.js
    Former-commit-id: 069d5689c184394065ad7f51e223c7420738ea6a [formerly 48be81824341e3d25f68f3e69bf98c2245829c9f] [formerly 7b39f037b2d782c456a45be0ccab17f3ec47f773 [formerly 7916bbf348842b3a1b86af57797421a5ff2205c2]] [formerly 848415c6940b343c74807f51337d9ea4995cd126 [formerly 18028287e462cec167950e3d263e42061902533a] [formerly cab2344286e38d7d0ab9acb01c5f1c31b22fc565 [formerly a7e0948020e41694cf57c153fd8c730a737aeea8]]]
    Former-commit-id: 26edd5fcc1216ea0c38a5c68c61472719eed0de7 [formerly 2fc89d584d281ac1515f5907ba2f44cfdb5bad67] [formerly 90f1f93844767aa751389021eab55a52bfcb3a1c [formerly c9a41aec35d322e6b2f2cb921eac5d067622279e]]
    Former-commit-id: e34cfa10369e2cb37790c6032299cf6589704497 [formerly 6ba519f4fb5ff2a36fb219065ce7735c345d2091]
    Former-commit-id: 118527455e46d9b0146f18ede45372be8f06bc78

--- main.js
+++ main.js
@@ -17,28 +17,33 @@
 */
 
 "use strict";
-const electron = require('electron');
-const app = electron.app; // Module to control application life.
-const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
+const {electron} = require('electron');
+const {app} = require('electron');  // Module to control application life.
+const {BrowserWindow} = require('electron'); // Module to create native browser window.
+
 //The ipcMain module, when used in the main process,
 //handles asynchronous and synchronous messages sent from a renderer process (web page).
-const ipcMain = require('electron').ipcMain;
-const shell = require('electron').shell; // Module that provides functions related to desktop integration.
-const globalShortcut = electron.globalShortcut;// module can register/unregister a global keyboard shortcut with the operating system so that you can customize the operations for various shortcuts.
+const {ipcMain} = require('electron');
+const {shell} = require('electron'); // Module that provides functions related to desktop integration.
+const {globalShortcut} = require('electron'); // module can register/unregister a global keyboard shortcut with the operating system so that you can customize the operations for various shortcuts.
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+let mainWindow = null;
 // The menu class is used to create native menus that can be used as application menus and context menus.
-var Menu = require('menu');
+const {Menu} = require('electron');
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-	app.quit();
+app.on('window-all-closed', () => {
+       // On macOS it is common for applications and their menu bar
+       // to stay active until the user quits explicitly with Cmd + Q
+       if (process.platform !== 'darwin'){
+       app.quit();
+       }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', () => {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({width: 1280, height: 800, javascritp: true, title: 'Teleprompter', useContentSize: true, nodeIntegration: true});
 
@@ -46,16 +51,15 @@ app.on('ready', function() {
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
     
   // Disables menu in systems where it can be disabled.
-  Menu.setApplicationMenu(null);
+    Menu.setApplicationMenu(null);
     
 	// Send a message to the renderer process...
-	ipcMain.on('asynchronous-message', function(event, arg) {
+	ipcMain.on('asynchronous-message', (event, arg) => {
 		event.sender.send('asynchronous-reply', 'Done');
 	});
-    
 
   // Register a 'F8' shortcut listener.
-  var ret = globalShortcut.register('F8', function() {
+  let ret = globalShortcut.register('F8', () => {
     mainWindow.openDevTools();
   });
   
@@ -67,7 +71,7 @@ app.on('ready', function() {
   // Check whether a shortcut is registered.
   console.log(globalShortcut.isRegistered('F8'));
 
-  app.on('will-quit', function() {
+  app.on('will-quit', () => {
   // Unregister a shortcut.
   globalShortcut.unregister('F8');
 
@@ -76,7 +80,10 @@ app.on('ready', function() {
 });
 
 	// Emitted when the window is closed.
-	mainWindow.on('closed', function(){
+	mainWindow.on('closed', () =>{
+        // Dereference the windows object, usually you would store  windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
 		mainWindow = null;
 	});
 });
