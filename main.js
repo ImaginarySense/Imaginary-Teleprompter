aaaaf734939678bb2f25fc07eda1fd25fa3e90ca
Javier O. Cordero Pérez <javier@imaginary.tech>
2016-10-07 01:44:26 -0400

    Fix windows build too...
    Former-commit-id: ad599ec139276820f7af901fe1f4aa4f152f1a93 [formerly 3d475da472ac5e38b9a45060bb41aab38f49ee40] [formerly 481ed35dbd9cec3b731f63b193346bee100b4107 [formerly 30674e0334327855bea7e821869dd4d57ae6605e]] [formerly 309e82e370331a3e0e99a710bd7464ff9bd1e122 [formerly 1dd8579dbf9664c22d699140e744e300e8ecadd7] [formerly 3c612ad8a376a05885b7757126fdeb38455bba32 [formerly 154a5fe4f1bfbe15df722299615cb70805749a3d]]]
    Former-commit-id: 57306f60ff7c2eae887725e564aaa2e1a7355108 [formerly 63d111b25af0a36a42b8f2596e236f5ee203ae5e] [formerly cd209ff590fd60b19aa0995b308aa870cbdff541 [formerly 7f6fc77c85bc939f7c658bb403de494e178ed00c]]
    Former-commit-id: 48642ed4acfc56b90090e41c10b8c105862a9624 [formerly c27136b8a7b0ecc25cacd3c8879455cf72fac0fa]
    Former-commit-id: d18623f4e3809743f04e14a68f0a6af5defc41b1

--- main.js
+++ main.js
@@ -17,6 +17,7 @@
 */
 
 "use strict";
+if (require('electron-squirrel-startup')) return;
 const {electron} = require('electron');
 const {app} = require('electron');  // Module to control application life.
 const {BrowserWindow} = require('electron'); // Module to create native browser window.
@@ -31,6 +32,75 @@ const {globalShortcut} = require('electron'); // module can register/unregister 
 let mainWindow = null;
 // The menu class is used to create native menus that can be used as application menus and context menus.
 const {Menu} = require('electron');
+
+const app = require('app');
+
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+const ChildProcess = require('child_process');
+const path = require('path');
+
+const appFolder = path.resolve(process.execPath, '..');
+const rootAtomFolder = path.resolve(appFolder, '..');
+const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+const exeName = path.basename(process.execPath);
+
+const spawn = function(command, args) {
+let spawnedProcess, error;
+
+try {
+	spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+} catch (error) {}
+
+return spawnedProcess;
+};
+
+const spawnUpdate = function(args) {
+return spawn(updateDotExe, args);
+};
+
+const squirrelEvent = process.argv[1];
+switch (squirrelEvent) {
+case '--squirrel-install':
+case '--squirrel-updated':
+// Optionally do things such as:
+// - Add your .exe to the PATH
+// - Write to the registry for things like file associations and
+//   explorer context menus
+
+// Install desktop and start menu shortcuts
+spawnUpdate(['--createShortcut', exeName]);
+
+setTimeout(app.quit, 1000);
+return true;
+
+case '--squirrel-uninstall':
+// Undo anything you did in the --squirrel-install and
+// --squirrel-updated handlers
+
+// Remove desktop and start menu shortcuts
+spawnUpdate(['--removeShortcut', exeName]);
+
+setTimeout(app.quit, 1000);
+return true;
+
+case '--squirrel-obsolete':
+// This is called on the outgoing version of your app before
+// we update to the new version - it's the opposite of
+// --squirrel-updated
+
+app.quit();
+return true;
+}
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
