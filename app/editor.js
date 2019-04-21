6c802dccbc9c419cc3d625654094deef5cd20566
javiercordero <javier.cordero@upr.edu>
2019-04-21 15:05:45 -0400

    Changed ClassicEditor for DecoupledEditor

--- app/editor.js
+++ app/editor.js
@@ -13,7 +13,7 @@ import 'bootstrap';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import DecoupledEditor from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Font from '@ckeditor/ckeditor5-font/src/font';
 // import FontFamilyConfig from '@ckeditor/ckeditor5-font/src/font';
@@ -54,8 +54,8 @@ class Main {
   // }
 }
 
-ClassicEditor
-  .create( document.querySelector('#prompter'), {
+DecoupledEditor
+  .create( document.getElementById('prompter'), {
     plugins: [
       Alignment,
       // Autosave,
@@ -80,26 +80,27 @@ ClassicEditor
       Undo
     ],
     toolbar: {
-      items: [/*'anchor','|',*/'undo','redo','|',/*'heading','|',*/'bold','italic','underline','strikethrough','|','subscript','superscript','|','FontFamily','FontSize','|','FontColor',/*'FontBackgroundColor',*/'highlight','|','removeFormat','|','numberedList','bulletedList','|','alignment','|','link','|','imageupload','insertTable','|'],
-      // https://ckeditor.com/docs/ckeditor5/latest/api/module_ui_panel_sticky_stickypanelview-StickyPanelView.html#member-viewportTopOffset
-      limiterElement: document.getElementById("siteTop"),
-      viewportTopOffset: 48
-    },
+      items: [/*'anchor','|',*/'undo','redo','|',/*'heading','|',*/'bold','italic','underline','strikethrough','|','subscript','superscript','|','FontFamily','FontSize','|','FontColor',/*'FontBackgroundColor',*/'highlight','|','removeFormat','|','numberedList','bulletedList','|','alignment','|','link','|','imageupload','insertTable','|']
+      // ClassicEditor https://ckeditor.com/docs/ckeditor5/latest/api/module_ui_panel_sticky_stickypanelview-StickyPanelView.html#member-viewportTopOffset
+      // limiterElement: document.getElementById("siteTop"),
+      // viewportTopOffset: toolbarTopOffset
+    // },
     // fontSize: '0.7 em/0.7em;0.8 em/0.8em;0.9 em/0,9em;1.0 em/1em;1.1 em/1.1em;1.2 em/1.2em;1.3 em/1.3em;1.4 em/1.4em;1.5 em/1.5em;1.6 em/1.6em;1.7 em/1.7em;1.8 em/1.8em;1.9 em/1.9em;2.0 em/2em;2.1 em/2.1em;2.2 em/2.2em;2.3 em/2.3em;2.4 em/2.4em;2.5 em/2.5em;2.6 em/2.6em;2.7 em/2.7em;2.8 em/2.8em;2.9 em/2.9em;3.0 em/3em',
     // line_height: '1 em/1em;1.1 em/1.1em;1.2 em/1.2em;1.3 em/1.3em;1.4 em/1.4em;1.5 em/1.5em;1.6 em/1.6em;1.7 em/1.7em;1.8 em/1.8em;1.9 em/1.9em;2.0 em/2.0em',
     // image: {
       // toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ],
-    // },
+    },
     autosave: {
       save( editor ) {
           // The saveData() function must return a promise
           // which should be resolved when the data is successfully saved.
           // return saveData( editor.getData() );
       }
-    },
+    }
   } )
   .then( editor => {
     console.log( 'Editor was initialized', editor );
+    document.getElementById("toolbar").appendChild(editor.ui.view.toolbar.element);
   } )
   .catch( error => {
     console.error( error.stack );
