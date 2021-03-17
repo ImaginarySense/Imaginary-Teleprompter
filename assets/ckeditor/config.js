e94b1ad5a7cfde73caedff7469075ba266a9bb36
va2ron1 <va2ron1@nuisx.com>
2021-03-16 21:30:50 -0400

    added system fonts experiments

--- assets/ckeditor/config.js
+++ assets/ckeditor/config.js
@@ -10,4 +10,48 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// config.filebrowserUploadUrl = 'http://localhost:3001/upload';
 	config.imageUploadUrl = 'http://localhost:3001/upload?command=QuickUpload'; 
+	
 };
+
+// Import fonts
+// const SomeNameFontFace = new FontFace("SomeName", 'url(/path/font.ttf)');
+// document.fonts.add(SomeNameFontFace);
+// SomeNameFontFace.loaded.then((fontFace) => {
+// 	// This is where you can declare a new font-family, because the font is now loaded and ready.  
+// 	console.info('Current status', fontFace.status);
+// 	console.log(fontFace.family, 'loaded successfully.');
+// 	// Throw an error if loading wasn't successful
+// }, (fontFace) => {
+// 	console.error('Current status', fontFace.status);
+// });
+// SomeNameFontFace.load();
+
+
+// Add system fonts to CKEditor
+if (navigator.userAgent.indexOf("Electron") != -1) {
+	const getSystemFonts = require('get-system-fonts');
+	const path = require('path');
+	
+	async function loadEditorConfig() {
+		// In an async function...
+		const files = await getSystemFonts();
+	
+		var fonts = [];
+		for (var i = 0; i < files.length; i++) {
+			let fontName = path.parse(files[i]).name.match(/[A-Z]+(?![a-z])|[A-Z]?[a-z]+|\d+/g).join(' ');
+			fonts.push(fontName);
+		}
+	
+		fonts = fonts.join(';');
+	
+		CKEDITOR.config.font_names = fonts;
+	
+	}
+	
+	loadEditorConfig();
+}
+
+
+
+
+
